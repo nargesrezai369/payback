dcc2ed4077eb07a7eef615d99c462e69b705724d
narges rezai <43164553+nargesrezai369@users.noreply.github.com>
2022-12-02 17:08:47 +0330

    Add files via upload

---
+++ src/test/java/com/business/payback/PaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.payback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaybackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
